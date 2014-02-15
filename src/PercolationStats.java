ceec6fab2e9a513056f7377ccfe5d95293f0a2d7
Alisson Perez <alissonperez@gmail.com>
2014-02-15 01:37:52 -0200

    Incluida versão inicial da PercolationStats

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,7 +1,45 @@
 public class PercolationStats {
 
+	public PercolationStats( int n , int t ) {
+		// Criação do Percolation...
+	}
+
+	public double mean() {
+		return 0.5929934999999997 ; // Valor fake, inicialmente...
+	}
+
+	public double stddev() {
+		return 0.00876990421552567 ; // Valor fake, inicialmente...
+	}
+
+	public double confidenceLo() {
+		return 0.666217665216461 ;
+	}
+
+	public double confidenceHi() {
+		return 0.6676773347835391 ;
+	}
+
 	public static void main( String[] args ) {
-		System.out.println( "It's alive..." ) ;
+		if ( args.length < 2 ) {
+			throw new IllegalArgumentException( "É preciso informar valores para N e T." ) ;
+		}
+
+		Integer n = Integer.parseInt( args[0] ) ;
+		Integer t = Integer.parseInt( args[1] ) ;
+
+		PercolationStats p = new PercolationStats( n , t ) ;
+
+		System.out.print( "mean                    = " ) ;
+		System.out.println( p.mean() ) ;
+
+		System.out.print( "stddev                  = " ) ;
+		System.out.println( p.stddev() ) ;
+
+		System.out.print( "95% confidence interval = " ) ;
+		System.out.print( p.confidenceLo() ) ;
+		System.out.print( ", " ) ;
+		System.out.println( p.confidenceHi() ) ;
 	}
 
 }
