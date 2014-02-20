96df7511d3cce0de0b8d00bb779b061ddd34d75f
Alisson Perez <alissonperez@gmail.com>
2014-02-19 21:52:09 -0300

    Incluido codestyle exigido pelo coursera

--- src/Percolation.java
+++ src/Percolation.java
@@ -7,132 +7,132 @@
  */
 public class Percolation {
 
-	private int n ; // Size
+    private int n; // Size
 
-	private boolean[] open_sites ; // Nós abertos.
+    private boolean[] openSites; // Nós abertos.
 
-	private WeightedQuickUnionUF uf ; // UnionFind implementation
+    private WeightedQuickUnionUF uf; // UnionFind implementation
 
-	private int top_site ; // Nó superior que liga toda a linha superior do grid
-	private int bottom_site ; // Nó inferior que liga toda a linha inferior do grid
+    private int topSite; // Nó superior que liga toda a linha superior do grid
+    private int bottomSite; // Nó inferior que liga toda a linha inferior do grid
 
-	/**
-	 * Create N-by-N grid, with all sites blocked
-	 *
-	 * @param  newN Tamanho
-	 * @return
-	 */
-	public Percolation( int newN ) {
-		n = newN ;
-		initialize() ;
-	}
+    /**
+     * Create N-by-N grid, with all sites blocked
+     *
+     * @param  newN Tamanho
+     * @return
+     */
+    public Percolation(int newN) {
+        n = newN;
+        initialize();
+    }
 
-	/**
-	 * Inicializa
-	 */
-	private void initialize() {
-		int n2 = n*n ;
-		int i ;
+    /**
+     * Inicializa
+     */
+    private void initialize() {
+        int n2 = n*n;
+        int i;
 
-		open_sites = new boolean[n2] ;
-		for ( i = 0 ; i < n2 ; i++ ) {
-			open_sites[i] = false ;
-		}
+        openSites = new boolean[n2];
+        for (i = 0; i < n2; i++) {
+            openSites[i] = false;
+        }
 
-		top_site = n2 ;
-		bottom_site = n2 + 1 ;
+        topSite = n2;
+        bottomSite = n2 + 1;
 
-		// Adiciona-se 2 para incluir o nó superior e inferior
-		uf = new WeightedQuickUnionUF( n2+2 ) ;
-		for ( i = 0 ; i < n ; i++ ) {
-			uf.union( top_site , i ) ; // Linha superior
-			uf.union( bottom_site , n2-i-1 ) ; // Linha inferior
-		}
-	}
+        // Adiciona-se 2 para incluir o nó superior e inferior
+        uf = new WeightedQuickUnionUF(n2+2);
+        for (i = 0; i < n; i++) {
+            uf.union(topSite , i); // Linha superior
+            uf.union(bottomSite , n2-i-1); // Linha inferior
+        }
+    }
 
-	/**
-	 * Open site (row i, column j) if it is not already
-	 *
-	 * @param i Row
-	 * @param j Column
-	 */
-	public void open( int i , int j )  {
-		if ( isOpen( i , j ) ) return ;
+    /**
+     * Open site (row i, column j) if it is not already
+     *
+     * @param i Row
+     * @param j Column
+     */
+    public void open(int i , int j)  {
+        if (isOpen(i , j)) return;
 
-		open_sites[ getSiteIdx( i , j ) ] = true ;
-		connectNeighborsIfOpen( i , j ) ;
-	}
+        openSites[ getSiteIdx(i , j) ] = true;
+        connectNeighborsIfOpen(i , j);
+    }
 
-	/**
-	 * Conecta o nó aos seus vizinhos, se estiveram abertos..
-	 *
-	 * @param i Linha
-	 * @param j Coluna
-	 */
-	private void connectNeighborsIfOpen( int i , int j )  {
-		int siteIdx = getSiteIdx( i , j ) ;
+    /**
+     * Conecta o nó aos seus vizinhos, se estiveram abertos..
+     *
+     * @param i Linha
+     * @param j Coluna
+     */
+    private void connectNeighborsIfOpen(int i , int j)  {
+        int siteIdx = getSiteIdx(i , j);
 
-		// Baixo
-		if ( i > 1 && isOpen( i-1 , j ) ) uf.union( siteIdx , getSiteIdx( i-1 , j ) ) ;
+        // Baixo
+        if (i > 1 && isOpen(i-1 , j)) uf.union(siteIdx , getSiteIdx(i-1 , j));
 
-		// Cima
-		if ( i < n && isOpen( i+1 , j ) ) uf.union( siteIdx , getSiteIdx( i+1 , j ) ) ;
+        // Cima
+        if (i < n && isOpen(i+1 , j)) uf.union(siteIdx , getSiteIdx(i+1 , j));
 
-		// Esquerda
-		if ( j > 1 && isOpen( i , j-1 ) ) uf.union( siteIdx , getSiteIdx( i , j-1 ) ) ;
+        // Esquerda
+        if (j > 1 && isOpen(i , j-1)) uf.union(siteIdx , getSiteIdx(i , j-1));
 
-		// Direita
-		if ( j < n && isOpen( i , j+1 ) ) uf.union( siteIdx , getSiteIdx( i , j+1 ) ) ;
-	}
+        // Direita
+        if (j < n && isOpen(i , j+1)) uf.union(siteIdx , getSiteIdx(i , j+1));
+    }
 
-	/**
-	 * Is site (row i, column j) open?
-	 *
-	 * @param  i Linha
-	 * @param  j Coluna
-	 * @return boolean
-	 */
-	public boolean isOpen( int i , int j )  {
-		return open_sites[getSiteIdx( i , j )] ;
-	}
+    /**
+     * Is site (row i, column j) open?
+     *
+     * @param  i Linha
+     * @param  j Coluna
+     * @return boolean
+     */
+    public boolean isOpen(int i , int j)  {
+        return openSites[getSiteIdx(i , j)];
+    }
 
-	/**
-	 * Is site (row i, column j) full? (Tem ligação com a linha superior?)
-	 *
-	 * @param  i Linha
-	 * @param  j Coluna
-	 * @return boolean
-	 */
-	public boolean isFull( int i , int j ) {
-		int siteIdx = getSiteIdx( i , j ) ;
-		return open_sites[siteIdx] && uf.connected( siteIdx , top_site ) ;
-	}
+    /**
+     * Is site (row i, column j) full? (Tem ligação com a linha superior?)
+     *
+     * @param  i Linha
+     * @param  j Coluna
+     * @return boolean
+     */
+    public boolean isFull(int i , int j) {
+        int siteIdx = getSiteIdx(i , j);
+        return openSites[siteIdx] && uf.connected(siteIdx , topSite);
+    }
 
-	/**
-	 * Retorna o índice para o elemento no array dadas as coordenadas.
-	 *
-	 * @param  i Linha
-	 * @param  j Coluna
-	 * @return int
-	 */
-	private int getSiteIdx( int i , int j ) {
-		if ( i < 1 || i > n || j < 1 || j > n ) {
-			throw new IndexOutOfBoundsException(
-				"As coordenadas 'i' e 'j' devem estar dentro do grid (NxN)."
-			) ;
-		}
+    /**
+     * Retorna o índice para o elemento no array dadas as coordenadas.
+     *
+     * @param  i Linha
+     * @param  j Coluna
+     * @return int
+     */
+    private int getSiteIdx(int i , int j) {
+        if (i < 1 || i > n || j < 1 || j > n) {
+            throw new IndexOutOfBoundsException(
+                "As coordenadas 'i' e 'j' devem estar dentro do grid (NxN)."
+            );
+        }
 
-		// Transformar as cordenadas i e j no indíce do array
-		return ( j-1 ) + ( i-1 ) * n ;
-	}
+        // Transformar as cordenadas i e j no indíce do array
+        return (j-1) + (i-1) * n;
+    }
 
-	/**
-	 * Does the system percolate?
-	 *
-	 * @return boolean
-	 */
-	public boolean percolates() {
-		return uf.connected( top_site , bottom_site ) ;
-	}
+    /**
+     * Does the system percolate?
+     *
+     * @return boolean
+     */
+    public boolean percolates() {
+        return uf.connected(topSite , bottomSite);
+    }
 
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -7,134 +7,134 @@
  */
 public class PercolationStats {
 
-	// Grid size (n) and num. of operations (t)
-	private int n ;
-	private int t ;
+    // Grid size (n) and num. of operations (t)
+    private int n;
+    private int t;
 
-	private double mean ;
-	private double stddev ;
-	private double[] confidence ;
+    private double mean;
+    private double stddev;
+    private double[] confidence;
 
-	private boolean _initialized ;
+    private boolean initialized;
 
-	/**
-	 * Perform T independent computational experiments on an N-by-N grid
-	 *
-	 * @param  newN Size
-	 * @param  newT Operations
-	 * @return
-	 */
-	public PercolationStats( int newN , int newT ) {
-		this.n = newN ;
-		this.t = newT ;
+    /**
+     * Perform T independent computational experiments on an N-by-N grid
+     *
+     * @param  newN Size
+     * @param  newT Operations
+     * @return
+     */
+    public PercolationStats(int newN , int newT) {
+        this.n = newN;
+        this.t = newT;
 
-		_initialized = false ;
-		if ( n <= 0 || t <= 0 )
-			throw new IllegalArgumentException( "N e T devem ser superiores à 0" ) ;
-	}
+        initialized = false;
+        if (n <= 0 || t <= 0)
+            throw new IllegalArgumentException("N e T devem ser superiores à 0");
+    }
 
-	/**
-	 * Sample mean of percolation threshold
-	 *
-	 * @return double
-	 */
-	public double mean() {
-		initialize() ;
-		return mean ;
-	}
+    /**
+     * Sample mean of percolation threshold
+     *
+     * @return double
+     */
+    public double mean() {
+        initialize();
+        return mean;
+    }
 
-	/**
-	 * Sample standard deviation of percolation threshold
-	 *
-	 * @return double
-	 */
-	public double stddev() {
-		initialize() ;
-		return stddev ;
-	}
+    /**
+     * Sample standard deviation of percolation threshold
+     *
+     * @return double
+     */
+    public double stddev() {
+        initialize();
+        return stddev;
+    }
 
-	/**
-	 * Returns lower bound of the 95% confidence interval
-	 *
-	 * @return double
-	 */
-	public double confidenceLo() {
-		initialize() ;
-		return confidence[0] ;
-	}
+    /**
+     * Returns lower bound of the 95% confidence interval
+     *
+     * @return double
+     */
+    public double confidenceLo() {
+        initialize();
+        return confidence[0];
+    }
 
-	/**
-	 * Returns upper bound of the 95% confidence interval
-	 *
-	 * @return double
-	 */
-	public double confidenceHi() {
-		initialize() ;
-		return confidence[1] ;
-	}
+    /**
+     * Returns upper bound of the 95% confidence interval
+     *
+     * @return double
+     */
+    public double confidenceHi() {
+        initialize();
+        return confidence[1];
+    }
 
-	/**
-	 * Inicializa a classe
-	 */
-	private void initialize() {
-		if ( _initialized ) {
-			return ;
-		}
+    /**
+     * Inicializa a classe
+     */
+    private void initialize() {
+        if (initialized) {
+            return;
+        }
 
-		double[] fractions = getFractions() ;
+        double[] fractions = getFractions();
 
-		mean = StdStats.mean( fractions ) ;
-		stddev = StdStats.stddev( fractions ) ;
+        mean = StdStats.mean(fractions);
+        stddev = StdStats.stddev(fractions);
 
-		confidence = new double[2] ;
-		confidence[0] = mean - ( 1.96 * stddev / Math.sqrt( t ) ) ;
-		confidence[1] = mean + ( 1.96 * stddev / Math.sqrt( t ) ) ;
+        confidence = new double[2];
+        confidence[0] = mean - (1.96 * stddev / Math.sqrt(t));
+        confidence[1] = mean + (1.96 * stddev / Math.sqrt(t));
 
-		_initialized = true ;
-	}
+        initialized = true;
+    }
 
-	/**
-	 * Retorna as frações de sites abertos em relação ao tamanho do grid para o número de operações (t)
-	 * Ex:
-	 * Grid de tamanho 20x20 (400 casas)
-	 * O grid filtra (de "percolates") ao abrir a 240th casa, portanto a fração é 0.51 (204/400)
-	 * @return double[]
-	 */
-	private double[] getFractions() {
-		double[] fractions = new double[t] ;
-		int n2 , operations , row , col ;
+    /**
+     * Retorna as frações de sites abertos em relação ao tamanho do grid para o número de operações (t)
+     * Ex:
+     * Grid de tamanho 20x20 (400 casas)
+     * O grid filtra (de "percolates") ao abrir a 240th casa, portanto a fração é 0.51 (204/400)
+     * @return double[]
+     */
+    private double[] getFractions() {
+        double[] fractions = new double[t];
+        int n2 , operations , row , col;
 
-		n2 = n*n ;
-		operations = 0 ;
+        n2 = n*n;
+        operations = 0;
 
-		Percolation o ;
+        Percolation o;
 
-		for ( int i = 0 ; i < t ; i++ ) {
-			operations = 0 ;
-			o = new Percolation( n ) ;
+        for (int i = 0; i < t; i++) {
+            operations = 0;
+            o = new Percolation(n);
 
-			while ( ! o.percolates() ) {
-				row = StdRandom.uniform( n ) + 1 ;
-				col = StdRandom.uniform( n ) + 1 ;
+            while (! o.percolates()) {
+                row = StdRandom.uniform(n) + 1;
+                col = StdRandom.uniform(n) + 1;
 
-				if ( o.isOpen( row , col ) ) continue ;
+                if (o.isOpen(row , col)) continue;
 
-				operations++ ;
-				o.open( row , col ) ;
-			}
+                operations++;
+                o.open(row , col);
+            }
 
-			fractions[i] = operations / (double) n2 ;
-		}
+            fractions[i] = operations / (double) n2;
+        }
 
-		return fractions ;
-	}
+        return fractions;
+    }
 
-	public static void main( String[] args ) {
-		PercolationStats ps = new PercolationStats( Integer.parseInt( args[0] ) , Integer.parseInt( args[1] ) ) ;
+    public static void main(String[] args) {
+        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]) , Integer.parseInt(args[1]));
 
-		System.out.printf( "%-24s = %f\n" , "mean" , ps.mean() ) ;
-		System.out.printf( "%-24s = %f\n" , "stdev" , ps.stddev() ) ;
-		System.out.printf( "%-24s = %f, %f\n" , "95% confidence interval" , ps.confidenceLo() , ps.confidenceHi() ) ;
-	}
+        System.out.printf("%-24s = %f\n" , "mean" , ps.mean());
+        System.out.printf("%-24s = %f\n" , "stdev" , ps.stddev());
+        System.out.printf("%-24s = %f, %f\n" , "95% confidence interval" , ps.confidenceLo() , ps.confidenceHi());
+    }
 
 }
